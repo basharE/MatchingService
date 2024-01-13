Fixing issue of rotation, enhancements on response to ui

deciding_model/Model_Trainer.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 import xgboost as xgb
+from bson import ObjectId
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -16,6 +17,10 @@ from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn import tree, neighbors
 from sklearn.metrics import f1_score
 from sklearn.model_selection import cross_val_score
+
+from configuration.ConfigurationService import get_database_uri_from_conf, get_database_name_from_conf, \
+    get_database_collection_name_from_conf
+from db.MongoConnect import connect_to_collection
 from deciding_model.Db_to_df_converter import get_from_mongo_to_dataframe
 from deciding_model.ResultEvaluator import ResultEvaluator
 
@@ -52,7 +57,7 @@ class ClassifierTrainer(metaclass=SingletonMeta):
         return f"TP: {_TP}, FP: {_FP}, TN: {_TN}, FN: {_FN}"
 
     def predict_all(self, input_df, _class):
-        X = input_df.iloc[:, 1:]
+        X = input_df.iloc[:, 2:]
         scaler = preprocessing.MinMaxScaler()
         X_normalized = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
         summed_probs = np.zeros((X_normalized.shape[0], 2))  # Adjust num_classes accordingly
@@ -65,14 +70,30 @@ class ClassifierTrainer(metaclass=SingletonMeta):
             summed_probs += probs
             logging.info(f"Prediction Result of {clf.__class__.__name__} is {probs}")
         evaluator = ResultEvaluator()
-        return evaluator.evaluate(props_dic, _class)
+        evaluator_res = evaluator.evaluate(props_dic, _class)
+        if evaluator_res != None:
+            id_of_object = input_df.loc[evaluator_res, 'id']
+            document_id = ObjectId(id_of_object)
+
+            collection = connect_to_collection(get_database_uri_from_conf(), get_database_name_from_conf(),
+                                               get_database_collection_name_from_conf())
+            document = collection.find_one({"_id": document_id},
+                                           {'_id': 0, 'name': 1})
+
+            if document:
+                # Access the value of the specific field
+                field_value = document.get("name")
+                return field_value
+            else:
+                return None
+        return evaluator_res
 
     def train_best_classifier(self, new):
         # Get dataframe from the database
         data_frame = get_from_mongo_to_dataframe(new)
 
         y = data_frame['class']
-        X = data_frame.iloc[:, 1:]
+        X = data_frame.iloc[:, 2:]
 
         # Normalize data
         scaler = preprocessing.MinMaxScaler()

services/ImageFindService.py
@@ -1,4 +1,3 @@
-import numpy as np
 from flask import jsonify
 
 from configuration.ConfigurationService import get_image_directory_from_conf
@@ -11,7 +10,6 @@ import io
 import base64
 import logging
 from PIL import Image
-import matplotlib.pyplot as plt
 
 
 def handle_request(request):
@@ -39,7 +37,7 @@ def get_result_of_prediction(data):
         return "Can't find image\n"
     if data == -1:
         return "Found more than one candidate, try again\n"
-    return "Image found in index, " + str(data) + "\n"
+    return "Your position is near, " + str(data) + "\n"
 
 
 def get_image_features_(request):
@@ -64,9 +62,9 @@ def get_image_features(request):
     # Rotate the image
     rotated_image = decompressed_image.rotate(270)
 
-    # Display the image using matplotlib
-    plt.imshow(np.array(rotated_image))
-    plt.show()
+    # # Display the image using matplotlib
+    # plt.imshow(np.array(rotated_image))
+    # plt.show()
 
     # Get the rotated image as bytes
     rotated_image_bytes = rotated_image.tobytes()
@@ -96,10 +94,10 @@ def handle_request_of_optimized_image(request):
 
         # Convert the image data to a PIL Image
         image = Image.open(io.BytesIO(image_data))
-        rotated_image = image.rotate(270)
+        rotated_image = image
 
-        plt.imshow(np.array(rotated_image))
-        plt.show()
+        # plt.imshow(np.array(rotated_image))
+        # plt.show()
 
         # return jsonify({'message': 'Image received successfully.'}), 200
         image_wrapper = ImageWrapper(rotated_image, filename="input_image.jpg")